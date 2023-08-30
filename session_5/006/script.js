abff7cf97897a8db6f98197e309c74d43022f8b6
Gleison Lemos <gleison04lemos@gmail.com>
2023-08-30 20:19:25 -0300

    feat: Variables in JavaScript

---
+++ session_5/006/script.js
@@ -0,0 +1,22 @@
+var nome = "João Ribeiro" // string
+var idade = 21  // number
+var administrador = false // boolean
+
+console.log(nome)
+
+var cidade 
+cidade = "Lisboa"
+
+var valor1 = 100, nome2 = "Ana", apelido = "Silva"
+
+//let e const
+let texto = "Frase de um texto"
+const pi = 3.14
+
+texto = "Nada"
+pi = 25
+
+// regras
+var let = 10 // não declarar variáveis com palavras reservadas
+var nome = "Gleison"    // As duas variáveis são diferentes
+var NOME = "Gleison"    // cas-sensitive 
