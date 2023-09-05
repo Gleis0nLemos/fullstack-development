a1b0254758ef1404ff5c550cd7ef008fafef9499
Gleison Lemos <gleison04lemos@gmail.com>
2023-09-05 11:11:16 -0300

    feat: Other Get Elements Methods

---
+++ session_5/028/script.js
@@ -0,0 +1,15 @@
+/*
+document.getElementsByClassName()
+document.getElementsByName()
+document.getElementsByTagName()
+*/
+
+// let e = document.getElementsByClassName("outra-noticia")
+// console.log(e)
+// e[2].innerText = "Nova notícia"
+
+// let e = document.getElementsByName("text-usuario")
+// e[0].value = "João"
+
+let e = document.getElementsByTagName("div")
+console.log(e)
