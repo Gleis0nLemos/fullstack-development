45ef9251ffd29ef3784c52d78330b9fd4c5b3e41
Gleison Lemos <gleison04lemos@gmail.com>
2024-09-23 22:08:53 -0300

    feat: exercise 22

---
+++ session_6/ex_022/app.js
@@ -0,0 +1,49 @@
+/* ----------------------------------------------------------------------------
+
+Exercício: 022
+Enunciado:
+    Está disponível um elemento select com 3 opções.
+    Por baixo existem 4 checkboxes.
+    Quando é selecionada a opção 3 no select, as checkboxes devem ficar ativas.
+    Qualquer outra opção, devem desativar as checkboxes.
+---------------------------------------------------------------------------- */
+
+// MINHA RESOLUCAO
+
+const selectOptions = document.querySelector('.form-select');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+checkboxes.forEach(checkbox => checkbox.disabled = true);
+
+selectOptions.addEventListener('change', () => {
+    if (selectOptions.value === '3') {
+        checkboxes.forEach(checkbox => {
+            checkbox.disabled = false
+        });
+    } else {
+        checkboxes.forEach(checkbox => checkbox.disabled = true);
+    }
+});
+
+// RESOLUCAO DO PROFESSOR
+
+// checks_state('disabled');
+
+// document.querySelector('[name="select_opcoes"]').addEventListener('change', (e) => {
+//     if (e.target.value == 3) {
+//         checks_state('enabled');
+//     } else {
+//         checks_state('disabled');
+//     }
+// })
+
+// function checks_state(state) {
+//     if (state == 'disabled') {
+//         document.querySelectorAll('[id^="check_"]').forEach( e => {
+//             e.setAttribute('disabled', true)
+//         });
+//     } else {
+//         document.querySelectorAll('[id^="check_"]').forEach( e => {
+//             e.removeAttribute('disabled')
+//         })
+//     }
+// }
