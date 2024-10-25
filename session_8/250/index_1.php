125777db2bd8a470b61c652c64e5847c73517af8
Gleison Lemos <gleison04lemos@gmail.com>
2024-10-25 15:36:07 -0300

    feat: Comentários em PHP

---
+++ session_8/250/index_1.php
@@ -0,0 +1,21 @@
+<?php
+
+// single-line comment
+
+# single-line comment
+
+/* multiple-line 
+comment */
+
+// ---------------------------
+// Comentários podem entrar aqui
+// ---------------------------
+
+# ---------------------------
+# Comentários podem entrar aqui
+# ---------------------------
+
+/* ---------------------------
+Comentários podem entrar aqui
+E nesta segunda linha também
+--------------------------- */
