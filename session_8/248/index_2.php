b5d456ab7f06cdf8eed0a5f825930d53633d2443
Gleison Lemos <gleison04lemos@gmail.com>
2024-10-25 14:50:43 -0300

    feat: Escrever Código de PHP

---
+++ session_8/248/index_2.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+
+  <h1>HTML</h1>
+
+  <?php
+  echo 'Hello World!';
+  ?>
+
+  <!-- O mesmo q o echo -->
+  <P><?= 'Hello World!' ?></P>
+
+  <div>
+    <?php
+      print 'Hello World!';
+    ?>
+  </div>
+</body>
+
+</html>
