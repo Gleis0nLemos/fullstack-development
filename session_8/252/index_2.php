4012882c095f10861754cef0a8bedbb4472144ec
Gleison Lemos <gleison04lemos@gmail.com>
2024-10-25 16:09:21 -0300

    feat: Atribuir Valores a Variáveis

---
+++ session_8/252/index_2.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    .cor-texto {
+      color: red;
+    }
+  </style>
+</head>
+<body>
+    
+    <h1>O meu nome é Gleison!</h1>
+
+    <?php
+      $nome = 'Gleison Lemos';
+    ?>
+
+    <h3>O meu nome é <?php echo $nome; ?>!</h3>
+    <h3>O meu nome é <?= $nome; ?>!</h3>
+
+    <!-- escrever HTML a partir do PHP -->
+    <?php 
+      echo "<p>Vamos criar um parágrafo de HTML!</p>";
+      echo '<p class="cor-texto">Este parágrafo tem uma classe de CSS.</p>';
+    ?>
+</body>
+</html>
